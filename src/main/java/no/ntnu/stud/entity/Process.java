f227784aa53eb03e070c51f776277285f2a61fa9
Henrik <henaa@stud.ntnu.no>
2025-03-10 09:44:21 +0100

    Process class

--- src/main/java/no/ntnu/stud/entity/Process.java
+++ src/main/java/no/ntnu/stud/entity/Process.java
@@ -1,4 +1,26 @@
 package no.ntnu.stud.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Process {
+
+    private int id;
+    private int arrivalTime;
+    private int burstTime;
+    private int priority;
+
+    @Override
+    public String toString() {
+        return "Process{" +
+            "id=" + id +
+            ", arrivalTime=" + arrivalTime +
+            ", burstTime=" + burstTime +
+            ", priority=" + priority +
+            '}';
+    }
 }
