7e5261ee6e98a29cf55fe1ab5073ebb9bf64b513
Henrik <henaa@stud.ntnu.no>
2025-03-10 10:04:51 +0100

    Added comment to process

--- src/main/java/no/ntnu/stud/algorithms/FirstComeFirstServe.java
+++ src/main/java/no/ntnu/stud/algorithms/FirstComeFirstServe.java
@@ -1,4 +1,14 @@
 package no.ntnu.stud.algorithms;
 
+import java.util.List;
+import no.ntnu.stud.entity.Process;
+
 public class FirstComeFirstServe {
+  List<Process> processes;
+
+    public FirstComeFirstServe(List<Process> processes) {
+        this.processes = processes;
+    }
+
+
 }
