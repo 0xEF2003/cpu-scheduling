15d92f25cc824250a2b4aebc32bb695bb2d2fa73
Henrik <henaa@stud.ntnu.no>
2025-03-10 10:43:39 +0100

    Algorithms should now be correct

--- src/main/java/no/ntnu/stud/algorithms/FirstComeFirstServe.java
+++ src/main/java/no/ntnu/stud/algorithms/FirstComeFirstServe.java
@@ -1,14 +1,38 @@
 package no.ntnu.stud.algorithms;
 
+import java.util.Comparator;
 import java.util.List;
 import no.ntnu.stud.entity.Process;
 
 public class FirstComeFirstServe {
   List<Process> processes;
 
-    public FirstComeFirstServe(List<Process> processes) {
-        this.processes = processes;
+  public FirstComeFirstServe(List<Process> processes) {
+    this.processes = processes;
+  }
+
+  public void sortProcesses() {
+    processes.sort(Comparator.comparingInt(Process::getArrivalTime));
+  }
+
+  public void run() {
+    int time = 0;
+    while (!processes.isEmpty()) {
+      sortProcesses();
+      Process process = processes.getFirst();
+      if (time < process.getArrivalTime()) {
+        time = process.getArrivalTime();
+      }
+      try {
+        Thread.sleep(process.getBurstTime());
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        System.out.println("Process " + process.getId() + " was interrupted.");
+        return;
+      }
+      time += process.getBurstTime();
+      System.out.println("Process " + process.getId() + " finished at " + time);
+      processes.removeFirst();
     }
-
-
+  }
 }
--- src/main/java/no/ntnu/stud/algorithms/PreEmptivePrioritySchedulingAlgorithm.java
+++ src/main/java/no/ntnu/stud/algorithms/PreEmptivePrioritySchedulingAlgorithm.java
@@ -1,4 +1,42 @@
 package no.ntnu.stud.algorithms;
 
+import java.util.List;
+import no.ntnu.stud.entity.Process;
+
 public class PreEmptivePrioritySchedulingAlgorithm {
+  private List<Process> processes;
+
+  public PreEmptivePrioritySchedulingAlgorithm(List<Process> processes) {
+    this.processes = processes;
+  }
+
+  public void sortProcesses() {
+    processes.sort((p1, p2) -> {
+      if (p1.getPriority() == p2.getPriority()) {
+        return p1.getArrivalTime() - p2.getArrivalTime();
+      }
+      return p2.getPriority() - p1.getPriority();
+    });
+  }
+
+  public void run() {
+    int time = 0;
+    while (!processes.isEmpty()) {
+      sortProcesses();
+      Process currentProcess = processes.getFirst();
+      if (time < currentProcess.getArrivalTime()) {
+        time = currentProcess.getArrivalTime();
+      }
+      try {
+        Thread.sleep(currentProcess.getBurstTime());
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        System.out.println("Process " + currentProcess.getId() + " was interrupted.");
+        return;
+      }
+      time += currentProcess.getBurstTime();
+      System.out.println("Process " + currentProcess.getId() + " finished at " + time);
+      processes.removeFirst();
+    }
+  }
 }
