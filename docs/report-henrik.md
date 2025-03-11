ca7cbd750627395f16bd23bd613c5a51cd63bab6
Henrik <henaa@stud.ntnu.no>
2025-03-11 15:08:24 +0100

    Some of the progress on my report

---
+++ docs/report-henrik.md
@@ -0,0 +1,13 @@
+# Contribution
+
+During this project, both I and Elias sat in a discord call working on the project.
+We have both written code and discuss the project.
+
+We tried to bounce the ball on who worked on the project back and forth, but the workload was not evenly distributed.
+
+# What has been learnt
+
+During this project I have learnt the different CPU scheduling algorithms and how they work.
+We decided to implement the Round Robin algorithm, and SFJ as well to get a deeper understanding of all the algorithms
+you have mentioned in class.
+
--- src/main/java/no/ntnu/stud/entity/SimulationResult.java
+++ src/main/java/no/ntnu/stud/entity/SimulationResult.java
@@ -1,21 +1,24 @@
 package no.ntnu.stud.entity;
 
+
+//TODO: Do final calulation of average waiting time and average turn around time, See if it is correct or not
+// https://process-scheduling-solver.boonsuen.com/
 public class SimulationResult {
-    private int averageWaitingTime;
-    private int averageTurnAroundTime;
+  private int averageWaitingTime;
+  private int averageTurnAroundTime;
 
-    public SimulationResult(int averageWaitingTime, int averageTurnAroundTime) {
-        this.averageWaitingTime = averageWaitingTime;
-        this.averageTurnAroundTime = averageTurnAroundTime;
-    }
+  public SimulationResult(int averageWaitingTime, int averageTurnAroundTime) {
+    this.averageWaitingTime = averageWaitingTime;
+    this.averageTurnAroundTime = averageTurnAroundTime;
+  }
 
-	public int getAverageWaitingTime() {
-		return averageWaitingTime;
-	}
+  public int getAverageWaitingTime() {
+    return averageWaitingTime;
+  }
 
-	public int getAverageTurnAroundTime() {
-		return averageTurnAroundTime;
-	}
+  public int getAverageTurnAroundTime() {
+    return averageTurnAroundTime;
+  }
 
 
 }
